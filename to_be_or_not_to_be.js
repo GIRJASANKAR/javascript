d8db749bf750e0610d4132f61304e0851ded42ff
GIRJA <st6164649@gmail.com>
2023-08-16 08:56:49 +0530

    complete

--- to_be_or_not_to_be.js
+++ to_be_or_not_to_be.js
@@ -1,8 +1,12 @@
 var expect = function(val) {
-    const obj={
-        toBe:function (val){
-            
+    return {
+        toBe:function (val1){
+        if(val1!==val) throw new Error("Not Equal");
+        else return true;
         },
-        notToBe:function (val){},
+        notToBe:function (val2){
+          if(val===val2) throw new Error("Equal");
+         else return true;
+        },
     }
 };
