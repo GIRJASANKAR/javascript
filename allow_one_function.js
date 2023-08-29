36afe82b103e87a7fc2bb3d8dfc81884e2bb7cae
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2023-08-29 12:07:37 +0530

    format

--- allow_one_function.js
+++ allow_one_function.js
@@ -2,15 +2,11 @@
  * @param {Function} fn
  * @return {Function}
  */
-var once = function(fn) {
-    
+var once = function(fn) { 
     let x = false;
-
    return (...args) => {
        if (x) return;
-
        x = true;
-
        return fn(...args);
    }
 };
