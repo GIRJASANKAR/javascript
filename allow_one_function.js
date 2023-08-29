402dc14fb6628b18246f1c4887345bac3a8faf2b
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2023-08-29 12:07:17 +0530

    allow_one_function

---
+++ allow_one_function.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    
+    let x = false;
+
+   return (...args) => {
+       if (x) return;
+
+       x = true;
+
+       return fn(...args);
+   }
+};
