9206d6351e65b3254ea930ffb14ff58d501c48a8
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2023-09-01 10:02:37 +0530

    first_n_last

---
+++ first_n_last.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(numberkaarray, final) {
+    return [numberkaarray.indexOf(final), numberkaarray.lastIndexOf(final)];
+};
