e2328233d107a52e2c8f07a94952ec8ac1335c05
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2023-09-03 18:46:43 +0530

    count_substring.js

---
+++ count_substring.js
@@ -0,0 +1,20 @@
+var countSubstrings = function(s, t) {
+    let res=0;
+    for(let i=0;i<s.length;i++){
+        for(let j=0;j<t.length;j++){
+            let diff=0;
+            let x=i;
+            let y=j;
+            while(x<s.length && y<t.length){
+                if(s[x]!==t[y]){
+                    diff++
+                }
+                if(diff===1)res++
+                if(diff>1) break;
+                x++
+                y++
+            }
+        }
+    }
+    return res;
+};
