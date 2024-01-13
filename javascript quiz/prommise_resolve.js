8f8f88f1d7dcf0d3cfa2c144ce1944c57213eda5
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2024-01-13 22:59:17 +0530

    previous value is used only

--- javascript quiz/prommise_resolve.js
+++ javascript quiz/prommise_resolve.js
@@ -2,7 +2,7 @@
 
 Promise.resolve(1)
 .then(() => 2)
-.then(3)
+.then(3)   // - this give me nothing hence previous value is used only
 .then((value) => value * 3)
 .then(Promise.resolve(4))
 .then(console.log)
