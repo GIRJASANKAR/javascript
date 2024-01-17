8b9f1fbc54ba4aac99e97b26e348a34400069215
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2024-01-17 11:23:14 +0530

    interval

---
+++ coding_question_GFE/cancellable_interval.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Function} callback
+ * @param {number} delay
+ * @param {...any} args
+ * @returns {Function}
+ */
+export default function setCancellableInterval(callback, delay, ...args) {
+  let setIntervalID= setInterval(callback, delay, ...args);
+  return (){
+    clearInterval(setIntervalID);
+  }
+}
